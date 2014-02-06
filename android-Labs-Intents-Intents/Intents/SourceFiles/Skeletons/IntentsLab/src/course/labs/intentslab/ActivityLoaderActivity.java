03091eb0e059a46e3512241d690b74c6cf29b522
Jerry Yuan <yhyuan@gmail.com>
2014-02-06 17:47:40 -0500

    Finish Intent lab

--- android-Labs-Intents-Intents/Intents/SourceFiles/Skeletons/IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
+++ android-Labs-Intents-Intents/Intents/SourceFiles/Skeletons/IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
@@ -66,10 +66,9 @@ public class ActivityLoaderActivity extends Activity {
 		Log.i(TAG,"Entered startExplicitActivation()");
 		
 		// TODO - Create a new intent to launch the ExplicitlyLoadedActivity class
-		
+		Intent intent = new Intent(ActivityLoaderActivity.this, ExplicitlyLoadedActivity.class);
 		// TODO - Start an Activity using that intent and the request code defined above
-
-
+		startActivityForResult(intent, GET_TEXT_REQUEST_CODE);
 	}
 
 	// Start a Browser Activity to view a web page or its URL
@@ -81,6 +80,8 @@ public class ActivityLoaderActivity extends Activity {
 		// TODO - Create a base intent for viewing a URL 
 		// (HINT:  second parameter uses parse() from the Uri class)
 		
+		Uri webpage = Uri.parse(URL);
+		Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
 		
 		// TODO - Create a chooser intent, for choosing which Activity
 		// will carry out the baseIntent. Store the Intent in the 
@@ -88,10 +89,14 @@ public class ActivityLoaderActivity extends Activity {
 		// createChooser())
 		
 		Intent chooserIntent = null;
-
+		// Create intent to show chooser
+		chooserIntent = Intent.createChooser(webIntent, CHOOSER_TEXT);
+		
 		Log.i(TAG,"Chooser Intent Action:" + chooserIntent.getAction());
 		// TODO - Start the chooser Activity, using the chooser intent
-		startActivity(chooserIntent);
+		if (webIntent.resolveActivity(getPackageManager()) != null) {
+			startActivity(chooserIntent);
+		}
 
 	}
 
@@ -103,6 +108,14 @@ public class ActivityLoaderActivity extends Activity {
 		// TODO - Process the result only if this method received both a
 		// RESULT_OK result code and a recognized request code
 		// If so, update the Textview showing the user-entered text.
+		   // Check which request we're responding to
+	    if (requestCode == GET_TEXT_REQUEST_CODE) {
+	        // Make sure the request was successful
+	        if (resultCode == RESULT_OK) {
+	        	String result = data.getStringExtra("result");
+	        	mUserTextView.setText(result);
+	        }
+	    }
 
 
 	}
